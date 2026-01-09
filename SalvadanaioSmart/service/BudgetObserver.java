cad04fbc65c5c843093d3d1eb0e10e717b1e4aa1
fehdamara <155977715+fehdamara@users.noreply.github.com>
2026-01-09 22:58:17 +0100

    Delete SalvadanaioSmart/service directory

--- SalvadanaioSmart/service/BudgetObserver.java
+++
@@ -1,6 +0,0 @@
-package service;
-
-public interface BudgetObserver {
-    void notificaSuperamento(double speso, double limite);
-}
-// This interface defines a method to notify when a budget limit is exceeded.
